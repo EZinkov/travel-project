3579f068e20a0c86eafed477907f2a6e1618ea8d
Evgeniy <evgeniyzinkov98@gmail.com>
2023-04-06 11:43:30 +0200

    add: Footer code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,138 @@
 import React from "react"
+import { FiSend } from "react-icons/fi"
+import { MdOutlineTravelExplore } from "react-icons/md"
+import { AiOutlineTwitter, AiFillYoutube } from "react-icons/ai"
+import { FaTripadvisor } from "react-icons/fa"
+import { FiChevronRight } from "react-icons/fi"
+
+import video from "../../assets/ocean-59476.mp4"
 import "./footer.scss"
+
 const Footer = () => {
-  return <></>
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video} loop autoPlay muted typeof="video/mp4"></video>
+      </div>
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter Email Adress" />
+            <button className="btn flex" type="sumbit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" /> Travel.
+              </a>
+            </div>
+            <div className="footerParagraph">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+            <div className="footerSocials flex">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdivosr
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>Made with ♡ by Evgeniy Zinkov</small>
+            <small>Copyrights RESERVED</small>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Footer
